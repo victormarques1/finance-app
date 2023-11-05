a515324bcad13f7e2a10fe900dab87765f412217
Victor Marques <marques.vct@hotmail.com>
2023-11-05 19:25:57 -0300

    feat: add UpdateUserService

--- src/services/create-user.js
+++ src/services/create-user.js
@@ -7,7 +7,6 @@ import { EmailAlreadyInUseError } from '../errors/user.js';
 
 export class CreateUserService {
     async execute(createUserParams) {
-        // TODO: verificar se o e-mail já está em uso
         const postgresGetUserByEmailRepository =
             new PostgresGetUserByEmailRepository();
 
---
+++ src/services/update-user.js
@@ -0,0 +1,44 @@
+import { EmailAlreadyInUseError } from '../errors/user.js';
+import { PostgresGetUserByEmailRepository } from '../repositories/postgres/get-user-by-email.js';
+import bcrypt from 'bcrypt';
+import { PostgresUpdateUserRepository } from '../repositories/postgres/update-user.js';
+
+export class UpdateUserService {
+    async execute(userId, updateUserParams) {
+        if (updateUserParams.email) {
+            const postgresGetUserByEmailRepository =
+                new PostgresGetUserByEmailRepository();
+
+            const userWithProvidedEmail =
+                await postgresGetUserByEmailRepository.execute(
+                    updateUserParams.email
+                );
+
+            if (userWithProvidedEmail) {
+                throw new EmailAlreadyInUseError(updateUserParams.email);
+            }
+        }
+
+        const user = {
+            ...updateUserParams,
+        };
+
+        if (updateUserParams.password) {
+            const hashedPassword = await bcrypt.hash(
+                updateUserParams.password,
+                10
+            );
+
+            user.password = hashedPassword;
+        }
+
+        const postgresUpdateUserRepository = new PostgresUpdateUserRepository();
+
+        const updateUser = await postgresUpdateUserRepository.execute(
+            userId,
+            user
+        );
+
+        return updateUser;
+    }
+}
