f16a4e0081da2da6f26833a0d39a76d364852c5b
Victor Marques <marques.vct@hotmail.com>
2023-11-10 09:29:35 -0300

    feat: add GetUserBalanceRepository

---
+++ src/repositories/postgres/user/get-user-balance.js
@@ -0,0 +1,15 @@
+import { PostgresHelper } from '../../../db/postgres/helper.js';
+
+export class PostgresGetUserBalanceRepository {
+    async execute(userId) {
+        const balance = await PostgresHelper.query(
+            `SELECT * from get_user_balance($1)`,
+            [userId]
+        );
+
+        return {
+            userId,
+            ...balance[0],
+        };
+    }
+}
